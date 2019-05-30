153266c51a31806d9125268075bd9c254c14b4ef
l123555 <1449053190@qq.com>
2019-05-30 19:27:39 +0800

    within 2km and parking

--- main/main.js
+++ main/main.js
@@ -1,4 +1,15 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+	var distance_price;
+	var time_price;
+	var price;
+	
+	if (inputs["distance"] <= 2){
+	    distance_price = 6;
+	}
+	
+	time_price = inputs["parkTime"]*0.25;
+	
+	price = Math.round(distance_price + time_price);
+    return price;
 };
